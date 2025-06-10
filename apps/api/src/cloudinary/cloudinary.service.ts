5071fa3d05fa57e64ac355e0eb4207e01cd705f0
AlexanderOI <vialexoriba1257@gmail.com>
2025-06-09 22:31:33 -0300

    feat(api): Add Cloudinary integration for image uploads; implement Cloudinary module and service; configure multer for image file handling.

---
+++ apps/api/src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common'
+import { File } from '@nest-lab/fastify-multer'
+import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'
+import sharp from 'sharp'
+
+@Injectable()
+export class CloudinaryService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    })
+  }
+
+  async uploadAvatar(file: File) {
+    const optimizedImage = await sharp(file.buffer)
+      .resize(512, 512, { fit: 'cover' })
+      .toFormat('webp')
+      .toBuffer()
+
+    const result: UploadApiResponse = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({}, (err, result) => {
+          if (err) reject(err)
+
+          if (result) resolve(result)
+          else reject(new Error('Failed to upload image'))
+        })
+        .end(optimizedImage)
+    })
+
+    return result.secure_url
+  }
+}
